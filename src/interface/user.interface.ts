6b3f226cb451661c5784871a5a15322531ef7cc6
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-02 18:11:35 +0545

    add: user interface configured

---
+++ src/interface/user.interface.ts
@@ -0,0 +1,11 @@
+export interface UserCredentials {
+    email: string,
+    password: string,
+}
+
+export interface IUser {
+    name: string;
+    phone: string;
+    email: string;
+    password: string;
+}
