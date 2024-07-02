5eeefa03b3960b8491f017f8297dde7634f5e67c
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-02 20:29:21 +0545

    feat: users model created

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,10 +1,13 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 import { ROLE } from "../constant/enum";
 import { Message } from "../constant/messages";
 import { emailAddressRegex, passwordRegex, phoneNumberRegex } from "../constant/regex";
 import { IUser } from "../interface/user.interface";
 
-const userSchema = new mongoose.Schema({
+// Interface for User
+interface IUserDocument extends Document, IUser {}
+
+const userSchema = new Schema<IUserDocument>({
     name: { type: String, required: true },
     email: {
         type: String,
@@ -19,19 +22,20 @@ const userSchema = new mongoose.Schema({
         match: [phoneNumberRegex, Message.validPhoneNumber]
     },
     password: {
-        type: String, required: true, match: [
-            passwordRegex,
-            Message.passwordShouldStrong
-        ]
+        type: String,
+        required: true,
+        match: [passwordRegex, Message.passwordShouldStrong]
     },
     role: {
-        type: ROLE,
+        type: String,
+        enum: Object.values(ROLE),
         default: ROLE.USER
     }
 }, {
     timestamps: true
 });
 
-const User = mongoose.model<IUser>('user', userSchema);
 
-module.exports = User
+const User = mongoose.model<IUserDocument>('User', userSchema);
+
+export default User;
