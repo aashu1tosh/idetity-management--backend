a035290a4fa0fc634927940ab8cdf4d99fa48e0e
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-02 20:26:32 +0545

    wip: class authcontroller created with create user method

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,37 @@
+import { type Request, type Response } from 'express';
+import { StatusCodes } from '../constant/statusCodes';
+// import { AuthService } from '../services/auth.service';
+import {
+    default as authServices
+} from '../services/auth.service';
+
+class AuthController {
+    async createUser(req: Request, res: Response) {
+        const response = await authServices.createUser(req.body);
+        res.status(StatusCodes.CREATED).json({
+            success: true,
+            message: 'Created Successfully',
+            data: response,
+        });
+    }
+
+    // async login(req: Request, res: Response) {
+    //     const response = await authServices.loginUser(req.body);
+    //     res.status(StatusCodes.SUCCESS).json({
+    //         success: true,
+    //         message: 'Login Successful',
+    //         main: response,
+    //     });
+    // }
+
+    // async updatePassword(req: Request, res: Response) {
+    //     const id = res?.locals?.id?.id;
+    //     await authService.updatePassword(req.body, id);
+    //     res.status(StatusCodes.SUCCESS).json({
+    //         success: true,
+    //         message: 'Password updated successfully',
+    //     });
+    // }
+}
+
+export default AuthController;
