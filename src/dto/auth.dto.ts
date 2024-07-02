e259718695320ec78ebfcc32b820ddb207a18327
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-02 20:27:01 +0545

    wip: auth dto created

---
+++ src/dto/auth.dto.ts
@@ -0,0 +1,48 @@
+import {
+    IsEmail,
+    IsEnum,
+    IsNotEmpty,
+    Length,
+    Matches
+} from 'class-validator';
+import { ROLE } from '../constant/enum';
+import { passwordRegex, phoneNumberRegex } from '../constant/regex';
+
+export class CreateUserDTO {
+    @IsNotEmpty()
+    @Length(5, 30)
+    name!: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email!: string;
+
+    @IsNotEmpty()
+    @Matches(passwordRegex, {
+        message:
+            'Password must contain at least one uppercase letter and one lowercase letter',
+    })
+    password!: string;
+
+    @IsNotEmpty()
+    @Matches(phoneNumberRegex, {
+        message: 'Enter a valid phone number',
+    })
+    phone!: string;
+
+}
+
+export class LoginUserDTO {
+    @IsNotEmpty()
+    @IsEmail()
+    email!: string;
+
+    @IsNotEmpty()
+    @Matches(passwordRegex, {
+        message:
+            'Password must contain at least one uppercase letter and one lowercase letter',
+    })
+    password!: string;
+}
+
+
