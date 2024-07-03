2ae965d34a067d8107a46b761f2251c7c156245b
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-03 08:11:12 +0545

    feat: webtoken services created

--- src/services/webtoken.service.ts
+++ src/services/webtoken.service.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { DotenvConfig } from '../config/env.config';
 
 class WebTokenServices {
+    // options are passed if refresh token should be generated too.
     sign(id: string) {
         const token = jwt.sign({ id }, DotenvConfig.JWT_SECRET as string, {
             expiresIn: DotenvConfig.JWT_TOKEN_EXPIRE,
