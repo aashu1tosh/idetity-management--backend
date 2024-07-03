3b1a5ab9af550e72dd2504aa87b16d9b59c924e0
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-03 08:07:33 +0545

    feat: admin controllers created

---
+++ src/controllers/admin.controller.ts
@@ -0,0 +1,40 @@
+import { type Request, type Response } from 'express';
+import { StatusCodes } from '../constant/statusCodes';
+import adminService from '../services/admin.service';
+import { getPaginationData, paginationValidator } from '../utils/pagination';
+
+class AdminController {
+    async getAll(req: Request, res: Response) {
+        const [page, perpage] = paginationValidator(
+            req.query.page as string,
+            req.query.perpage as string
+        );
+        const response = await adminService.getAll(page, perpage);
+        res.status(StatusCodes.SUCCESS).json({
+            success: true,
+            message: 'Fetch Successful',
+            data: {
+                data: response.data,
+                pagination: getPaginationData(response.total, page, perpage),
+            },
+        });
+    }
+
+    async resetPassword(req: Request, res: Response) {
+        await adminService.resetPassword(req.body);
+        res.status(StatusCodes.SUCCESS).json({
+            success: true,
+            message: 'Password Reset Successful',
+        });
+    }
+
+    async deleteUser(req: Request, res: Response) {
+        await adminService.deleteUser(req.params.id);
+        res.status(StatusCodes.SUCCESS).json({
+            success: true,
+            message: 'User Deletion Successful',
+        });
+    }
+}
+
+export default AdminController;
